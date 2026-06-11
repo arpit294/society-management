3013fd1aebca18cf967868cdf1be86a7f9c2ec7e
unknown <arpitvadhiyari84@gmail.com>
2026-06-11 11:40:39 +0530

    all

--- app/Models/FlatType.php
+++ app/Models/FlatType.php
@@ -11,13 +11,7 @@ class FlatType extends Model
 
     protected $fillable = [
         'name',
-<<<<<<< HEAD
         'maintenance_fee',
-=======
-        'owner_maintenance_fee',
-        'rental_maintenance_fee',
-        'penalty_per_day',
->>>>>>> main
         'description',
         'status',
     ];
